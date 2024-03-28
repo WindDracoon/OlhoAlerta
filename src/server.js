2a91b2cbe3f4f055cc659472f41c0880eeaf79f5
WindDracoon <jovito.souza44@gmail.com>
2024-03-27 21:24:48 -0300

    newcom

--- src/server.js
+++ src/server.js
@@ -4,13 +4,22 @@ import pgk from "bcryptjs"
 import {prisma} from './lib/prisma.js'
 import jwt from '@fastify/jwt'
 import {env} from "./env/index.js";
+import cors from '@fastify/cors'
 
 const {compare, hash} = pgk
 
 const app = fastify()
 
+app.register(cors, {
+    origin: '*',
+    methods: ['POST', 'GET']
+})
+
 app.register(jwt, {
-    secret: env.JWT_SECRET
+    secret: env.JWT_SECRET,
+    sign:{
+        expiresIn:'1d'
+    }
 })
 
 app.post('/users', async (request, reply) => {
@@ -48,6 +57,7 @@ app.post('/users', async (request, reply) => {
     return reply.status(201).send()
 })
 
+// Autenticação de usuário
 app.post('/authenticate', async (request, reply) => {
     try{
         const registerBodySchema = z.object({
@@ -83,6 +93,33 @@ app.post('/authenticate', async (request, reply) => {
 
     }catch{
         return reply.status(500).send({message:'Erro no servidor'})
+    }
+})
+
+// Dados do usuário
+app.get('/me', async(request, reply) => {
+    try{
+        await request.jwtVerify()
+
+        const user = await prisma.users.findUnique({
+            where:{
+                id: request.user.sub
+            }
+        })
+
+        if(!user){
+            return reply.status(409).send({message:'E-mail não existe'})
+        }
+
+        return reply.status(200).send({
+            user:{
+                ...user,
+                password_hash: undefined
+            }
+        })
+
+    }catch{
+        return reply.status(401).send({message:'Unauthorized'})
     }
 })
 
